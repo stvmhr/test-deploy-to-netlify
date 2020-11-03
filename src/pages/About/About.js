7f80063dfffd1d2270c7421dff8975da95cbc2f3
Steve Mahar <maharsteve@gmail.com>
2020-11-02 19:51:38 -0500

    add pages

---
+++ src/pages/About/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import Layout from "../../layouts/Layout";
+
+export default class About extends Component {
+  render() {
+    return <Layout>About page moi le sac</Layout>;
+  }
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import Layout from "../../layouts/Layout";
+
+export default class Home extends Component {
+  render() {
+    return <Layout>Home Page Patate Boi</Layout>;
+  }
+}
